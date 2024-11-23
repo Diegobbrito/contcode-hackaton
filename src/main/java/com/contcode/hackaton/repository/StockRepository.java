c76cc52096907a86090cbd997d3d5d9bd97955b2
Diego Brito <dibbrito@gmail.com>
2024-11-23 10:38:11 -0300

    add imports

--- src/main/java/com/contcode/hackaton/repository/StockRepository.java
+++ src/main/java/com/contcode/hackaton/repository/StockRepository.java
@@ -1,5 +1,7 @@
 package com.contcode.hackaton.repository;
 
+import org.springframework.stereotype.Repository;
+
 @Repository
 public class StockRepository {
 
--- src/main/java/com/contcode/hackaton/service/StockService.java
+++ src/main/java/com/contcode/hackaton/service/StockService.java
@@ -1,5 +1,5 @@
 package com.contcode.hackaton.service;
-
+import org.springframework.stereotype.Service;
 @Service
 public class StockService {
 
