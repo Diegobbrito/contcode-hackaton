083be1a36a850b4285afe17734ce750851e082dd
Diego Brito <dibbrito@gmail.com>
2024-11-22 23:55:23 -0300

    Add db schema

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS hackaton;
+USE hackaton;
+
+CREATE TABLE stock (
+    id INT NOT NULL AUTO_INCREMENT,
+    code VARCHAR(200) NOT NULL,
+    company_name VARCHAR(200) NOT NULL,
+    company_document VARCHAR(200) NOT NULL,
+    type VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
